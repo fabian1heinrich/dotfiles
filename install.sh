d1fd124e18e5bedb559cd9afafbf7044e3e970b7
Fabian Heinrich <fabianheinrich@aol.com>
2024-03-14 14:27:03 +0000

    init

---
+++ install.sh
@@ -0,0 +1,18 @@
+sudo apt update
+sudo apt install stow zsh fzf -y
+
+# oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+# fzf-tab
+git clone https://github.com/Aloxaf/fzf-tab ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/fzf-tab
+# zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+# starship
+sh -c "$(curl -fsSL https://starship.rs/install.sh)" "" -y
+
+# stow init
+rm ~/.zshrc
+stow -t ~ zsh
+stow -t ~/.config starship
+
+source ~/.zshrc
